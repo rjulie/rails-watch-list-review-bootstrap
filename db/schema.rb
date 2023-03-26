fb4ea8719bad149b6ecb59d807f5c64b8096ca66
Julie Reynal <rjulie.pro@gmail.com>
2023-03-26 11:37:43 +0200

    front design show list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_25_165558) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_26_081327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_25_165558) do
     t.string "title"
     t.text "overview"
     t.string "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
